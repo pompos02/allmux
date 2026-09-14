a5d791ef9b344cf704663752983cfe2955ad8758
pompos02 <yiannis.caravellas@gmail.com>
2026-09-14 22:14:33 +0300

    chore: remove debugging scripts

--- debug.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pid="$(pgrep -n -x allmux)"
-
-exec gdb -q -tui "build/debug/allmux" \
-  -ex "attach $pid" \
-  -ex "set scheduler-locking off" \
-  -ex "break src/tmux.hpp:86"
